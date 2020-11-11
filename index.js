4dca596497d85e2a24f1e0aa670e6e87bed7caf6
Your Name <you@example.com>
2020-11-11 14:12:12 -0600

    1st - Setup

--- index.js
+++ index.js
@@ -39,9 +39,29 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+// Person.prototype.eat = function(edible){
+//   if(this.stomach.length < 10){
+//     this.stomach.push(edible);
+//   }
+// }
+// Person.prototype.poop = function(){
+//   this.stomach = [];
+// }
+// Person.prototype.toString = function(){
+//   return `${this.name}, ${this.age}`;
+// }
+// const personOne = new Person('Christopher', 42);
+// console.log(personOne.toString());
+// personOne.eat('Tacos');
+// console.log(personOne.stomach);
+// personOne.poop();
+// console.log(personOne.stomach);
 
 /*
   TASK 2
